096a7b76537dd435f1fcc3dc0bbc49a36e9ee305
sambuite <murilosambuite@gmail.com>
2020-08-10 20:31:23 -0300

    remove routes files

--- src/routes/login.routes.ts
+++
@@ -1,9 +0,0 @@
-import { Router } from 'express';
-
-const loginRouter = Router();
-
-loginRouter.get('/', (req, res) => {
-  return res.send('Hello World');
-});
-
-export default loginRouter;
--- src/routes/registerUser.routes.ts
+++
@@ -1,9 +0,0 @@
-import { Router } from 'express';
-
-const registerUserRouter = Router();
-
-registerUserRouter.get('/', (req, res) => {
-  return res.send('Hello World');
-});
-
-export default registerUserRouter;
