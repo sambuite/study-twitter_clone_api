b9bcc77ac22be4bff0d29c857de521b1a2b8e53e
sambuite <murilosambuite@gmail.com>
2020-08-20 10:49:51 -0300

    refactor: remove files to remake project

--- src/routes/index.ts
+++
@@ -1,9 +0,0 @@
-import { Router } from 'express';
-
-import userRouter from './user.routes';
-
-const routes = Router();
-
-routes.use('/users', userRouter);
-
-export default routes;
--- src/server.ts
+++
@@ -1,3 +0,0 @@
-import app from './app';
-
-app.listen(3333, () => console.log(`Server up at port 3333`));
